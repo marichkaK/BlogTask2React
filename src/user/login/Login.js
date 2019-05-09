6628a1f32b12585ccb629a13981b7e6f7d8af7a2
marichkaK <maria199881@gmail.com>
2019-05-09 21:14:49 +0300

    add readme and delete useless frontend code

--- src/user/login/Login.js
+++ src/user/login/Login.js
@@ -1,11 +1,9 @@
 import React, {Component} from 'react';
 import './Login.css';
-import {ACCESS_TOKEN, FACEBOOK_AUTH_URL, GITHUB_AUTH_URL, GOOGLE_AUTH_URL} from '../../constants';
+import {ACCESS_TOKEN, GOOGLE_AUTH_URL} from '../../constants';
 import {login} from '../../util/APIUtils';
 import {Link, Redirect} from 'react-router-dom'
-import fbLogo from '../../img/fb-logo.png';
 import googleLogo from '../../img/google-logo.png';
-import githubLogo from '../../img/github-logo.png';
 import Alert from 'react-s-alert';
 
 class Login extends Component {
@@ -56,10 +54,6 @@ class SocialLogin extends Component {
             <div className="social-login">
                 <a className="btn btn-block social-btn google" href={GOOGLE_AUTH_URL}>
                     <img src={googleLogo} alt="Google"/> Log in with Google</a>
-                <a className="btn btn-block social-btn facebook" href={FACEBOOK_AUTH_URL}>
-                    <img src={fbLogo} alt="Facebook"/> Log in with Facebook</a>
-                <a className="btn btn-block social-btn github" href={GITHUB_AUTH_URL}>
-                    <img src={githubLogo} alt="Github"/> Log in with Github</a>
             </div>
         );
     }
